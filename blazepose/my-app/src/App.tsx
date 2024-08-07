b287e8a3f7a99aced7a45edd7f955fed91f3c154
dahyun <kdh5005@naver.com>
2024-08-07 15:42:40 +0900

    loading renewal

--- blazepose/my-app/src/App.tsx
+++ blazepose/my-app/src/App.tsx
@@ -15,7 +15,7 @@ const App: React.FC = () => {
         <Route path="/ranking" element={<Ranking />} />
         {/* <Route path="/neon" element={<Neon />} /> */}
         <Route path="/game" element={<GamePage />} />
-        {/* <Route path="/loading" element={<Loading />} /> */}
+        <Route path="/loading" element={<Loading />} />
 
       </Routes>
     </Router>
