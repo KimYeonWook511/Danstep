1bc967ec47c345f3994b905305c7a522e36fe5e6
김연욱 <ccoma511@naver.com>
2024-08-08 17:59:00 +0900

    메인 페이지 게임 리스트 조회 기능 구현 중

--- backend/src/main/java/com/danstep/game/model/service/GameService.java
+++ backend/src/main/java/com/danstep/game/model/service/GameService.java
@@ -2,7 +2,11 @@ package com.danstep.game.model.service;
 
 import com.danstep.game.model.dto.GameInfoDTO;
 
+import java.util.List;
+
 public interface GameService {
+    List<GameInfoDTO> getAllGames();
+
     GameInfoDTO getGameInfo(Integer id);
 
     Object getGamePose(Integer id);
