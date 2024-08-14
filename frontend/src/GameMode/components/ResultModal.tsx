d0144347c830599aa324a48d61866cb222ae4521
김연욱 <ccoma511@naver.com>
2024-08-14 13:54:33 +0900

    비로그인 시 로직 테스트

--- frontend/src/GameMode/components/ResultModal.tsx
+++ frontend/src/GameMode/components/ResultModal.tsx
@@ -42,11 +42,6 @@ const ResultModal: React.FC<ResultModalProps> = ({ isOpen, onClose, score, bad, 
 
     const accessToken = localStorage.getItem('accessToken') || '';
     setIsSubmitted(true); // 제출 완료 후 상태 업데이트
-    if (!accessToken) {
-      setShowLogin(true);
-      return;
-    }
-
     try {
       const data = {
         score,
@@ -67,8 +62,8 @@ const ResultModal: React.FC<ResultModalProps> = ({ isOpen, onClose, score, bad, 
       });
 
       console.log(response);
-      
     } catch (error) {
+      setIsSubmitted(false);
       console.error('Failed to submit result:', error);
     }
   };
