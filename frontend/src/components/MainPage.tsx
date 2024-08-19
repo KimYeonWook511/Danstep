98c7fd6be7c3c86b6f31321dba6a2f0d60a5e50e
KimYeonWook511 <kimyeonwook511@gmail.com>
2024-08-19 23:56:49 +0900

    임시 배포

--- frontend/src/components/MainPage.tsx
+++ frontend/src/components/MainPage.tsx
@@ -2,45 +2,38 @@ import React, { useEffect, useState } from 'react';
 import './MainPage.css';
 import Carousel3d from './Carousel3d';
 import NavBar from './NavBar';
-import Modal from './StartModal';
 import mainBackGround from '../assets/main_background.mp4';
 import api from '../api/api';
 import { Game } from './types';
 
-const MainPage: React.FC = () => {
+interface MainPageProps {
+  onMainMusicControl: (play: boolean) => void;
+}
+
+const MainPage: React.FC<MainPageProps> = ({ onMainMusicControl }) => {
   const [games, setGames] = useState<Game[]>([]);
 
   useEffect(() => {
     const fetchGames = async () => {
       try {
-        const response = await api.get("/games");
+        const response = await api.get('/games');
         const data = response.data;
         setGames(data);
       } catch (error) {
+        console.error('Failed to fetch games:', error);
       }
     };
 
     fetchGames();
   }, []);
 
-
   return (
-    <div className='main-page-container'>
+    <div className="main-page-container">
       <NavBar />
-      <video
-        autoPlay
-        loop
-        muted
-        className='background-video'
-      >
-        <source
-          src={mainBackGround}
-          type='video/mp4'
-        />
+      <video autoPlay loop muted className="background-video">
+        <source src={mainBackGround} type="video/mp4" />
       </video>
-      <Carousel3d
-        data={games}
-      />
+      <Carousel3d data={games} onMainMusicControl={onMainMusicControl} />
     </div>
   );
 };
